11c592d305bee2c7a21d0420e042cf12b515ae10
Manish8798 <mankum08071998@gmail.com>
2023-07-21 03:53:28 +0530

    home screen created

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -6,6 +6,7 @@ import {
   StyleSheet,
   TouchableWithoutFeedback,
   Keyboard,
+  Image,
 } from 'react-native';
 import {H, W, dimensions} from '../../util/Dimension';
 import {Colors} from '../../util/Constant';
@@ -13,11 +14,84 @@ import LinearGradient from 'react-native-linear-gradient';
 import {Input, Button} from '@rneui/themed';
 
 const Home = () => {
+  const handleTouchablePress = () => {
+    // Dismiss the keyboard when the user taps outside the TextInput
+    Keyboard.dismiss();
+  };
   return (
-    <SafeAreaView>
-      <Text>Home</Text>
+    <SafeAreaView style={styles.container}>
+      <TouchableWithoutFeedback onPress={handleTouchablePress}>
+        <LinearGradient colors={['#868BFE', '#EAF0FF']} style={styles.gradient}>
+          <View
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-start',
+              padding: 20,
+            }}>
+            <Image source={require('../../assets/avatar.png')} />
+            <Text style={styles.signupText}>Rohit Chu</Text>
+          </View>
+          <View style={styles.mainView}>
+            <Button
+              title={'CREATE'}
+              buttonStyle={{
+                backgroundColor: Colors.CreateButton,
+                borderRadius: 20,
+              }}
+              titleStyle={{fontWeight: 'bold', fontSize: 24}}
+              containerStyle={{
+                height: 50,
+                width: '75%',
+                marginVertical: 25,
+                alignSelf: 'center',
+              }}
+              onPress={() => console.log('create')}
+            />
+            <Image style={styles.image} source={require('../../assets/peep.png')} />
+            <Text style={styles.fadeText}>Create a task to get started</Text>
+          </View>
+        </LinearGradient>
+      </TouchableWithoutFeedback>
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  gradient: {
+    flex: 1,
+    width: '100%',
+  },
+  container: {
+    flex: 1,
+  },
+  mainView: {
+    height: '85%',
+    backgroundColor: 'white',
+    bottom: 0,
+    position: 'absolute',
+    width: '100%',
+    borderTopRightRadius: 40,
+    borderTopLeftRadius: 40,
+  },
+  signupText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: 'white',
+    fontFamily: 'Poppins-Regular',
+    paddingHorizontal: 10,
+  },
+  image: {
+    alignSelf: 'center'
+  },
+  fadeText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    textAlign: 'center',
+    color: '#D9D9D9'
+  }
+});
 
 export default Home;
--- util/Constant.js
+++ util/Constant.js
@@ -2,5 +2,7 @@ export const Colors = {
   HeadText: '#18306D',
   SubText: '#1B21B5',
   ButtonColor: '#868BFE',
-  DescTextColor: '#CDCFF6'
+  DescTextColor: '#CDCFF6',
+  CreateButton: '#F82626',
+  FadeText: '#D9D9D9'
 };
